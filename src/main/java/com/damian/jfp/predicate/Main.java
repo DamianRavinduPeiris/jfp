2a93ae8c2e21164fecc6c9dc4fff0c28e6ae4155
DamianRavinduPeiris <drpeiris3@gmail.com>
2024-08-08 12:52:35 +0530

    Added a logger and IntPredicate.

--- src/main/java/com/damian/jfp/predicate/Main.java
+++ src/main/java/com/damian/jfp/predicate/Main.java
@@ -1,21 +1,35 @@
 package com.damian.jfp.predicate;
 
 import java.util.ArrayList;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
+import java.util.logging.Logger;
 
 public class Main {
 
     public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Main.class.getName());
         var numbersList = new ArrayList<Integer>();
         numbersList.add(100);
         numbersList.add(141);
+
         Predicate<Integer> isOdd = (Integer i) -> i%2!=0;
         numbersList.forEach(n->{
             if(isOdd.test(n)){
-                System.out.println(n +" is Odd!");
+                logger.info(n +" is Odd!");
             }else{
-                System.out.println(n+" is Even!");
+                logger.info(n+" is Even!");
             }
+        });
+        // For int we can use IntPredicate also.
+        IntPredicate isOdd2 = (int i) -> i%2!=0;
+        numbersList.forEach(n->{
+            if(isOdd2.test(n)){
+                logger.info(n +" is Odd!");
+            }else{
+                logger.info(n+" is Even!");
+            }
+
         });
     }
 }
